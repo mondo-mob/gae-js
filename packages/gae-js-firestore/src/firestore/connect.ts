16815f50d0388cfcfd310ecc322d31b631c40d3f
Martin Levett <martin@dotrun.com.au>
2021-02-09 10:14:33 +1100

    Don't include host and port when connecting to real firestore

--- packages/gae-js-firestore/src/firestore/connect.ts
+++ packages/gae-js-firestore/src/firestore/connect.ts
@@ -6,18 +6,20 @@ export const connectFirestore = (settings?: Settings): Firestore => {
   const logger = createLogger("connectFirestore");
   const configuration = configurationStore.get<GaeJsFirestoreConfiguration>();
 
+  if (process.env.APP_ENGINE_ENVIRONMENT) {
+    logger.info("Running in deployed environment");
+    return new Firestore({
+      projectId: configuration.firestoreProjectId || undefined,
+      ...settings,
+    });
+  }
+
   const firestoreSettings: Settings = {
     projectId: configuration.firestoreProjectId,
     host: configuration.firestoreHost,
     port: configuration.firestorePort,
     ...settings,
   };
-
-  if (process.env.APP_ENGINE_ENVIRONMENT) {
-    logger.info("Running in deployed environment");
-    return new Firestore(firestoreSettings);
-  }
-
   logger.info(
     "Connecting to firestore in non-deployed environment: " +
       `${firestoreSettings.projectId}@${firestoreSettings.host}:${firestoreSettings.port}`
