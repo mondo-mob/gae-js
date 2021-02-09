cdab65c420d1e5b409e5ba9b66a556609e8cd67d
Martin Levett <martin@dotrun.com.au>
2021-02-09 21:53:36 +1100

    Fix express imports

--- packages/gae-js-core/src/middleware/handle-async.ts
+++ packages/gae-js-core/src/middleware/handle-async.ts
@@ -1,4 +1,4 @@
-import { NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 
 export const handleAsync = (handlerFn: (req: Request, res: Response, next?: NextFunction) => Promise<unknown>) => (
   req: Request,
