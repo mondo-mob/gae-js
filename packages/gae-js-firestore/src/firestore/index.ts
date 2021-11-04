a04b45fa1860b267c8e95c9fdad77ba5a5ffffde
Martin Levett <martin@mondo.ventures>
2021-11-05 09:55:58 +1100

    Add missing export

--- packages/gae-js-firestore/src/firestore/index.ts
+++ packages/gae-js-firestore/src/firestore/index.ts
@@ -1,6 +1,7 @@
 export * from "./connect";
 export * from "./firestore-loader";
 export * from "./firestore-query";
+export * from "./firestore-provider";
 export * from "./firestore-repository";
 export * from "./firestore-request-storage";
 export * from "./transactional";
