16c753f86bbedcb9fdb6ae6a889e56c4c0e43e16
Martin Levett <martin@mondo.ventures>
2022-05-10 11:28:28 +1000

    Add firestore repository tests for update, query select, query limit and offset, query ordering and query cursors.
    https://trello.com/c/x9XOsg1x

--- packages/gae-js-firestore/src/firestore/firestore-repository.test.ts
+++ packages/gae-js-firestore/src/firestore/firestore-repository.test.ts
@@ -17,10 +17,10 @@ const repositoryItemSchema = t.type({
 interface RepositoryItem {
   id: string;
   name: string;
+  prop1?: string;
+  prop2?: string;
+  prop3?: string;
 }
-
-// TODO: beforePersist hook
-// TODO: update
 
 describe("FirestoreRepository", () => {
   const collection = "repository-items";
@@ -358,6 +358,56 @@ describe("FirestoreRepository", () => {
     });
   });
 
+  describe("update", () => {
+    it("updates documents outside of transaction", async () => {
+      await repository.insert([createItem("123", { message: "create" }), createItem("234", { message: "create" })]);
+
+      await repository.update([createItem("123", { message: "update" }), createItem("234", { message: "update" })]);
+
+      const fetched = await repository.get(["123", "234"]);
+      expect(fetched.length).toBe(2);
+      expect(fetched[0]).toEqual({ id: "123", name: `Test Item 123`, message: "update" });
+    });
+
+    it("updates documents in transaction", async () => {
+      await repository.insert([createItem("123", { message: "create" }), createItem("234", { message: "create" })]);
+
+      await runWithRequestStorage(async () => {
+        firestoreLoaderRequestStorage.set(new FirestoreLoader(firestore));
+        return runInTransaction(() =>
+          repository.update([createItem("123", { message: "update" }), createItem("234", { message: "update" })])
+        );
+      });
+
+      const fetched = await repository.get(["123", "234"]);
+      expect(fetched.length).toBe(2);
+      expect(fetched[0]).toEqual({ id: "123", name: `Test Item 123`, message: "update" });
+    });
+
+    describe("with schema", () => {
+      beforeEach(async () => {
+        repository = new FirestoreRepository<RepositoryItem>(collection, {
+          firestore,
+          validator: repositoryItemSchema,
+        });
+        await repository.insert([createItem("123", { message: "create" }), createItem("234", { message: "create" })]);
+      });
+
+      it("updates document outside of transaction that matches schema", async () => {
+        await repository.update([createItem("123", { message: "update" }), createItem("234", { message: "update" })]);
+
+        const fetched = await repository.get(["123", "234"]);
+        expect(fetched.length).toBe(2);
+        expect(fetched[0]).toEqual({ id: "123", name: `Test Item 123`, message: "update" });
+      });
+
+      it("throws for document that doesn't match schema", async () => {
+        const abc = { id: "123", message: "no name" } as any as RepositoryItem;
+        await expect(repository.save(abc)).rejects.toThrow('"repository-items" with id "123" failed to save');
+      });
+    });
+  });
+
   describe("delete", () => {
     it("deletes a document outside of transaction", async () => {
       await firestore.doc(`${collection}/123`).create({
@@ -414,9 +464,6 @@ describe("FirestoreRepository", () => {
   });
 
   describe("query", () => {
-    // TODO: selects specific fields
-    // TODO: limit and offset
-    // TODO: ordering
     it("filters by exact match", async () => {
       await repository.save([createItem("123"), createItem("234")]);
 
@@ -432,6 +479,203 @@ describe("FirestoreRepository", () => {
 
       expect(results.length).toBe(1);
       expect(results[0].name).toEqual("Test Item 234");
+    });
+
+    it("selects specific fields", async () => {
+      await repository.save([
+        createItem("123", { prop1: "prop1", prop2: "prop2", prop3: "prop3" }),
+        createItem("234", { prop1: "prop1", prop2: "prop2", prop3: "prop3" }),
+      ]);
+
+      const results = await repository.query({
+        select: ["prop1", "prop3"],
+      });
+
+      expect(results.length).toBe(2);
+      expect(results[0].prop1).toEqual("prop1");
+      expect(results[0].prop2).toBeUndefined();
+      expect(results[0].prop3).toEqual("prop3");
+    });
+
+    describe("limit and offset", () => {
+      beforeEach(async () => {
+        await repository.save([
+          createItem("123", { prop1: "user1" }),
+          createItem("234", { prop1: "user2" }),
+          createItem("345", { prop1: "user3" }),
+          createItem("456", { prop1: "user4" }),
+          createItem("567", { prop1: "user5" }),
+        ]);
+      });
+
+      it("applies limit", async () => {
+        const results = await repository.query({
+          limit: 3,
+        });
+
+        expect(results.length).toBe(3);
+      });
+
+      it("applies offset", async () => {
+        const results = await repository.query({
+          offset: 3,
+        });
+
+        expect(results.length).toBe(2);
+        expect(results[0].id).toEqual("456");
+      });
+
+      it("applies limit and offset", async () => {
+        const results = await repository.query({
+          limit: 2,
+          offset: 2,
+        });
+
+        expect(results.length).toBe(2);
+        expect(results[0].id).toEqual("345");
+        expect(results[1].id).toEqual("456");
+      });
+    });
+
+    describe("ordering", () => {
+      beforeEach(async () => {
+        await repository.save([
+          createItem("123", { prop1: "AA", prop2: "XX" }),
+          createItem("234", { prop1: "BA", prop2: "XX" }),
+          createItem("345", { prop1: "AB", prop2: "ZZ" }),
+          createItem("456", { prop1: "BB", prop2: "YY" }),
+          createItem("567", { prop1: "CA", prop2: "XX" }),
+        ]);
+      });
+
+      it("orders results ascending", async () => {
+        const results = await repository.query({
+          sort: {
+            property: "prop1",
+            direction: "asc",
+          },
+        });
+
+        expect(results.length).toBe(5);
+        expect(results.map((doc) => doc.id)).toEqual(["123", "345", "234", "456", "567"]);
+      });
+
+      it("orders results descending", async () => {
+        const results = await repository.query({
+          sort: {
+            property: "prop1",
+            direction: "desc",
+          },
+        });
+
+        expect(results.length).toBe(5);
+        expect(results.map((doc) => doc.id)).toEqual(["567", "456", "234", "345", "123"]);
+      });
+
+      it("orders by multiple fields", async () => {
+        const results = await repository.query({
+          sort: [
+            {
+              property: "prop2",
+              direction: "asc",
+            },
+            {
+              property: "prop1",
+              direction: "desc",
+            },
+          ],
+        });
+
+        expect(results.length).toBe(5);
+        expect(results.map((doc) => doc.id)).toEqual(["567", "234", "123", "456", "345"]);
+      });
+
+      it("orders results by id special key", async () => {
+        const results = await repository.query({
+          sort: [{ property: "prop2" }, { property: "__name__" }],
+        });
+
+        expect(results.length).toBe(5);
+        expect(results.map((doc) => doc.id)).toEqual(["123", "234", "567", "456", "345"]);
+      });
+    });
+
+    describe("cursors", () => {
+      beforeEach(async () => {
+        await repository.save([
+          createItem("123", { prop1: "msg1" }),
+          createItem("234", { prop1: "msg2" }),
+          createItem("345", { prop1: "msg1" }),
+          createItem("456", { prop1: "msg2" }),
+          createItem("567", { prop1: "msg1" }),
+        ]);
+      });
+
+      it("applies startAfter", async () => {
+        const results = await repository.query({
+          sort: { property: "name" },
+          startAfter: ["Test Item 234"],
+        });
+
+        expect(results.length).toBe(3);
+        expect(results[0].id).toEqual("345");
+      });
+
+      it("applies startAt", async () => {
+        const results = await repository.query({
+          sort: { property: "name" },
+          startAt: ["Test Item 345"],
+        });
+
+        expect(results.length).toBe(3);
+        expect(results[0].id).toEqual("345");
+      });
+
+      it("applies endBefore", async () => {
+        const results = await repository.query({
+          sort: { property: "__name__" },
+          startAt: ["234"],
+          endBefore: ["567"],
+        });
+
+        expect(results.length).toBe(3);
+        expect(results[2].id).toEqual("456");
+      });
+
+      it("applies endAt", async () => {
+        const results = await repository.query({
+          sort: { property: "__name__" },
+          startAfter: ["234"],
+          endAt: ["567"],
+        });
+
+        expect(results.length).toBe(3);
+        expect(results[2].id).toEqual("567");
+      });
+
+      it("applies multiple properties", async () => {
+        const results = await repository.query({
+          sort: [{ property: "prop1" }, { property: "__name__" }],
+          startAfter: ["msg1", "345"],
+          limit: 2,
+        });
+
+        expect(results.length).toBe(2);
+        expect(results[0].id).toEqual("567");
+        expect(results[1].id).toEqual("234");
+      });
+
+      it("applies cursor and limit", async () => {
+        const results = await repository.query({
+          sort: { property: "__name__" },
+          startAfter: ["234"],
+          limit: 2,
+        });
+
+        expect(results.length).toBe(2);
+        expect(results[0].id).toEqual("345");
+        expect(results[1].id).toEqual("456");
+      });
     });
   });
 });
