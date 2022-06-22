0040e76d0cd8a7a6a4b9bdca1c3828b53890c3fe
Martin Levett <martin@mondo.ventures>
2022-06-23 09:27:08 +1000

    Remove default search id generator when preparing search index. Consumers must now pass id factory param.

---
+++ .changeset/swift-coats-approve.md
@@ -0,0 +1,5 @@
+---
+"@mondomob/gae-js-core": major
+---
+
+Remove default search id generator when preparing search index. Consumers must now pass id factory param.
--- packages/gae-js-core/src/search/searchable.ts
+++ packages/gae-js-core/src/search/searchable.ts
@@ -13,23 +13,19 @@ export interface Searchable<T> {
   search(searchFields: SearchFields, sort?: Sort, page?: Page): Promise<SearchResults<T>>;
 }
 
-const defaultIdFactory = <T extends { id: string }>(entity: T): string => {
-  return entity.id;
-};
-
 /**
  * Default indexing for search:
  * - for entries marked as "true" in the indexConfig, the raw entity values of the fields are used
  * - for entries configured with a function, the value is the result of passing the entity into the function
  * @param indexConfig the index configuration for this type of entity
  * @param entity entity to index
- * @param searchIdFactory optional function to convert entity to search id string
+ * @param searchIdFactory function to convert entity to search id string
  * @return search index entry
  */
-export const prepareIndexEntry = <T extends { id: string }>(
+export const prepareIndexEntry = <T>(
   indexConfig: IndexConfig<T>,
   entity: T,
-  searchIdFactory: (entity: T) => string = defaultIdFactory
+  searchIdFactory: (entity: T) => string
 ): IndexEntry => {
   const fields = Object.entries(indexConfig).reduce((acc, [fieldName, option]) => {
     acc[fieldName] = option === true ? (entity as any)[fieldName] : option(entity);
