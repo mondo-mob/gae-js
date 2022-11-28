6420c5711f8ae18bf2d69185d9434b9b614e197b
Matt Byrne <mbyrne00@gmail.com>
2022-11-28 15:22:45 +1100

    Slight typing improvement to isFirestoreError helper to tell compiler the assumed interface

---
+++ .changeset/brown-pianos-sort.md
@@ -0,0 +1,5 @@
+---
+"@mondomob/gae-js-firestore": patch
+---
+
+Slight typing improvement to isFirestoreError helper to tell compiler the assumed interface
--- packages/gae-js-firestore/src/firestore/firestore-errors.ts
+++ packages/gae-js-firestore/src/firestore/firestore-errors.ts
@@ -1,16 +1,26 @@
 import { StatusCode } from "@google-cloud/firestore/build/src/status-code";
 
-interface FirestoreError {
-  code: number;
+export interface FirestoreError {
+  code: StatusCode;
   details: string;
 }
 
-export const isFirestoreError = (err: unknown, type: StatusCode) => {
+/**
+ * Tries to guess if this looks like a Firestore error with the assumed type. This is because the firestore lib is not
+ * kind to us with types or providing more concrete fields to assert on.
+ *
+ * @param err   Unknown error we are checking
+ * @param code  Code for subtype error to check
+ *
+ * Also tells compile to assume the error is now of type FirestoreError.
+ */
+export const isFirestoreError = (err: unknown, code: StatusCode): err is FirestoreError => {
   if (!isFirestoreErrorShape(err)) {
     return false;
   }
 
-  return (err as FirestoreError).code === type;
+  return err.code === code;
 };
 
-const isFirestoreErrorShape = (err: any) => err?.code && err?.details;
+const isFirestoreErrorShape = (err: any): err is FirestoreError =>
+  typeof err?.code === "number" && typeof err?.details === "string";
